2d1f589812456b2696491e135e90ced0addedeee
MarkhusHounsou <hounsoumarkhus2004@gmail.com>
2026-04-09 19:13:08 +0200

    sae

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
+import { Platform } from 'react-native';
 
-// IMPORTANT: Remplace 192.168.1.X par ton IP locale IPv4
+const getBaseUrl = () => {
+  if (Platform.OS === 'web') return 'http://localhost:8080/api';
+  if (Platform.OS === 'android') return 'http://10.0.2.2:8080/api';
+  return 'http://192.168.1.148:8080/api'; // IP pour iOS ou Expo Go physique
+}
+
 const api = axios.create({
-  baseURL: 'http://192.168.1.155:8080/api', 
+  baseURL: getBaseUrl(), 
   headers: {
     'Content-Type': 'application/json',
   },
